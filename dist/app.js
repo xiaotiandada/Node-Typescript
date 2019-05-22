2d79f5831c1e6f8eaabf907c01a2d4ed1fd0fb20
xiaotiandada <952822399@qq.com>
2019-05-23 00:54:16 +0800

    🚀 add mongoose router

--- dist/app.js
+++ dist/app.js
@@ -3,8 +3,24 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+const body_parser_1 = __importDefault(require("body-parser"));
+const mongoose_1 = __importDefault(require("mongoose"));
 const express_1 = __importDefault(require("express"));
 const app = express_1.default();
-app.get('/', (req, res) => res.send('Hello World! 23333~ 自动编译刷新1222111'));
-app.listen(3000, () => console.log('Example app listening on port 3000!'));
+const index_1 = __importDefault(require("./router/index"));
+// Configure bodyparser to handle post requests
+app.use(body_parser_1.default.json());
+app.use(body_parser_1.default.urlencoded({ extended: true }));
+// Connect to MongoDB
+mongoose_1.default.connect('mongodb://localhost/restful')
+    .then(() => {
+    console.log('mongoose connect success');
+})
+    .catch((err) => {
+    console.log(`mongoose connect error ${err}`);
+});
+// Express configuration
+app.set('port', process.env.PORT || 3000);
+app.use('/api', index_1.default);
+app.listen(app.get('port'), () => console.log(`app listening on port ${app.get('port')}!`));
 //# sourceMappingURL=app.js.map
--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,28 @@
+import bodyParser from "body-parser";
+import mongoose from "mongoose";
+
 import {Express, Request, Response} from "express";
 import express from 'express'
 const app: Express = express();
+import router from './router/index'
 
-app.get('/', (req: Request, res: Response) => res.send('Hello World! 23333~ 自动编译刷新1222111'))
 
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+// Configure bodyparser to handle post requests
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// Connect to MongoDB
+mongoose.connect('mongodb://localhost/restful')
+.then(() => {
+  console.log('mongoose connect success')
+})
+.catch((err: any) => {
+  console.log(`mongoose connect error ${err}`);
+})
+
+// Express configuration
+app.set('port', process.env.PORT || 3000)
+app.use('/api', router)
+
+app.listen(app.get('port'), () => console.log(`app listening on port ${app.get('port')}!`))
